8898f3ef9d1a0b15b01890461559265dbb29056a
Nathan Nguyen <zhectx@gmail.com>
2021-10-06 15:40:20 -0400

    Initial Commit

---
+++ GameEngines3_Exercise3/Assets/Scripts/CharacterBehaviour.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    public float moveSpeed;
+    public float jumpForce;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+
+
+    //Private
+    private Rigidbody2D rb;
+    private float Direction;
+
+    //States
+    private bool isFacingRight = true;
+    private bool isJumping;
+    private bool isGrounded;
+    private bool isWalking;
+    
+
+    //Encounter
+    private bool GenerateEncounter = false;
+    private bool EncounterAvailable = true;
+    
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Inputs
+        InputHandler();
+
+        // Facing
+        if(Direction > 0 && !isFacingRight) {
+            Flip();
+        }
+        else if(Direction < 0 && isFacingRight) {
+            Flip();
+        }
+
+
+
+        //Check for Ground
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f,groundMask);
+
+    }
+
+    //Physics Handling
+    void FixedUpdate()
+    {
+        //Movement
+        Movement();
+
+        //Encounter
+        EncounterRNG();
+    }
+
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if(col.gameObject.name == "Props")
+        {
+            GenerateEncounter = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.gameObject.name == "Props")
+        {
+            EncounterAvailable = true;
+        }
+    }
+
+    private void EncounterRNG()
+    {
+        if (GenerateEncounter == true && isWalking == true && EncounterAvailable == true)
+        {
+            if(Random.Range(0,100) <= 1)
+            {
+                Debug.Log("Battle!");
+                GenerateEncounter = false;
+                EncounterAvailable = false;
+            }
+            GenerateEncounter = false;
+        }
+    }
+
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0.0f,180.0f,0.0f);
+    }
+
+
+
+    private void InputHandler()
+    {
+        Direction = Input.GetAxis("Horizontal");
+        if(Direction != 0)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+    }
+
+    private void Movement()
+    {
+        //Move
+        rb.velocity = new Vector2(Direction * moveSpeed, rb.velocity.y);
+
+        //Jumping
+        if(isJumping && isGrounded)
+        {
+            rb.AddForce(new Vector2(0.0f, jumpForce));
+        }
+        isJumping = false;
+    }
+
+
+
+}
